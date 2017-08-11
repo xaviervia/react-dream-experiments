646439ede22346ae40cfe35a245a33e7ff40c89f
Xavier Via <fernando.via@gmail.com>
2017-08-11 22:41:13 +0200

    Add index page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,38 @@
-import style from './index.css'
+import React from 'react'
+import {render} from 'react-dom'
+import {Html, of} from 'react-dream'
 
-const div = document.createElement('div')
-div.innerHTML = '<h1>Hello World</h1>'
-div.className = style.component
+const links = [
+  'with-store'
+]
 
-document.getElementById('root').appendChild(div)
+const withChild = Child => Parent => ({child, parent}) =>
+  <Parent {...parent}>
+    <Child {...child} />
+  </Parent>
+
+const nChildren = Item => Wrapper => ({items, wrapper}) =>
+  <Wrapper {...wrapper}>
+    {items.map((item, index) => <Item key={index} {...item} />)}
+  </Wrapper>
+
+const Link = of(withChild)
+  .ap(Html.A)
+  .ap(Html.Li)
+  .contramap(({name}) => ({
+    child: { href: `./${name}/`, children: name }
+  }))
+  .name('Link')
+
+const List = of(nChildren)
+  .ap(Link)
+  .ap(Html.Ul)
+  .contramap(({items}) => ({
+    items: items.map(name => ({name}))
+  }))
+  .name('List')
+
+render(
+  <List.Component items={links} />,
+  document.getElementById('root')
+)
