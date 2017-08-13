52e4fcdbb840001a64faf3e5cc0610c772d75e4d
Xavier Via <fernando.via@gmail.com>
2017-08-13 16:11:11 +0200

    Add Article to pointfree

--- src/pointfree-style/index.js
+++ src/pointfree-style/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import {render} from 'react-dom'
-import Header from './Header'
+import Article from './Article'
 
 render(
-  <Header.Component
+  <Article.Component
     title='lala'
-    tagline='lorem ipsum'
+    content='lorem ipsum'
   />,
   document.getElementById('root')
 )
